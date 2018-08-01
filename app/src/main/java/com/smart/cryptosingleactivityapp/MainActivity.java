03207d69ddba6f0c1e54507a452eba46d7c7557e
AdminVenkatesan <venkat.ramdoss@gmail.com>
2018-08-01 15:54:39 +0530

    Implemented RecyclerView&Volley

--- app/src/main/java/com/smart/cryptosingleactivityapp/MainActivity.java
+++ app/src/main/java/com/smart/cryptosingleactivityapp/MainActivity.java
@@ -8,10 +8,16 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -35,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
     private RequestQueue mQueue;
     private final ObjectMapper mObjMapper = new ObjectMapper();
     private String DATA_FILE_NAME = "crypto.data";
+    private MyCryptoAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,9 +53,11 @@ public class MainActivity extends AppCompatActivity {
     private void bindViews() {
         Toolbar toolbar = this.findViewById(R.id.toolbar);
         recView = this.findViewById(R.id.recView);
+        mAdapter = new MyCryptoAdapter();
         LinearLayoutManager lm = new LinearLayoutManager(this);
         lm.setOrientation(LinearLayoutManager.VERTICAL);
         recView.setLayoutManager(lm);
+        recView.setAdapter(mAdapter);
         setSupportActionBar(toolbar);
     }
 
@@ -129,8 +138,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private class EntityToModelMapperTask extends AsyncTask<List<CryptoCoinEntity>,Void,List<CoinModel>>{
-
-
         @Override
         protected List<CoinModel> doInBackground(List<CryptoCoinEntity>... data) {
             final ArrayList<CoinModel> listData = new ArrayList<>();
@@ -145,6 +152,8 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(List<CoinModel> data) {
             Log.d(TAG, "onPostExecute() called with: data = [" + data + "]");
+            mAdapter.setItems(data);
+            mAdapter.notifyDataSetChanged();
             super.onPostExecute(data);
         }
     }
@@ -165,4 +174,52 @@ public class MainActivity extends AppCompatActivity {
             this.volume24H = volume24H;
         }
     }
+
+    private static class MyCryptoAdapter extends RecyclerView.Adapter<MyCryptoAdapter.CoinViewHolder>{
+        List<CoinModel> mItems = new ArrayList<>();
+        public final String STR_TEMPLATE_NAME = "%s\t\t\t\t\t\t%s";
+        public final String STR_TEMPLATE_PRICE = "%s$\t\t\t\t\t\t24H Volume:\t\t\t%s$";
+
+        @Override
+        public CoinViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            //Log.d(TAG, "onBindViewHolder() called with: holder = " + "onCreateViewHolder");
+            return new CoinViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item,parent,false));
+        }
+
+        @Override
+        public void onBindViewHolder(CoinViewHolder holder, int position) {
+            final CoinModel model = mItems.get(position);
+            //Log.d(TAG, "onBindViewHolder() called with: holder = " + model.name);
+            holder.tvNameAndSymbol.setText(String.format(STR_TEMPLATE_NAME,model.name , model.symbol));
+            holder.tvPriceAndVolume.setText(String.format(STR_TEMPLATE_PRICE,model.priceUsd , model.volume24H));
+            Glide.with(holder.ivIcon).load(model.imageUrl).into(holder.ivIcon);
+        }
+
+        @Override
+        public int getItemCount() {
+            //Log.d(TAG, "setItems()" + mItems.size());
+            return mItems.size();
+        }
+
+        public void setItems(List<CoinModel> data){
+            //Log.d(TAG, "setItems()" + data.size());
+            this.mItems.clear();
+            this.mItems.addAll(data);
+        }
+
+        class CoinViewHolder extends RecyclerView.ViewHolder{
+
+            TextView tvNameAndSymbol;
+            TextView tvPriceAndVolume;
+            ImageView ivIcon;
+
+            public CoinViewHolder(View itemView) {
+                super(itemView);
+                tvNameAndSymbol = itemView.findViewById(R.id.tvNameAndSymbol);
+                tvPriceAndVolume = itemView.findViewById(R.id.tvPriceAndVolume);
+                ivIcon = itemView.findViewById(R.id.ivIcon);
+            }
+        }
+    }
+
 }
